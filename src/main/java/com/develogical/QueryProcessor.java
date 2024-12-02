ee49c410e5e3529f6e30e28a6856d71586926b1c
PeterNotFound114 <sicheng2@andrew.cmu.edu>
2024-12-02 11:46:16 -0500

    large num

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,6 +4,22 @@ public class QueryProcessor {
 
     public String process(String query) {
 
+        if (query.toLowerCase().contains("which one of the following numbers is the largest: ")) {
+            String[] numbers = query.split(":")[1].split(", ");
+            int max = Integer.MIN_VALUE;
+            for (String number : numbers) {
+                int num = Integer.parseInt(number);
+                if (num > max) {
+                    max = num;
+                }
+            }
+            return String.valueOf(max);
+        }
+
+        if (query.toLowerCase().contains("plus")) {
+            String[] numbers = query.split(" ")[3].split(" and ");
+        }
+
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
