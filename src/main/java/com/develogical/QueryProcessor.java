76c604841e92fd543b8e9c65099c9b0087007e3d
Om Patel <opatel@andrew.cmu.edu>
2024-12-02 11:22:22 -0500

    Updated Code for what is your name?

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -10,7 +10,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
 
-        if (query.contains("your name")) {
+        if (query.contains("What is your name?")) {
             return "RobTest";
         }
 
